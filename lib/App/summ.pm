8d70e3e680feda9ca2454c1bb536f4130f023df5
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-07-07 22:10:03 +0700

    Initial commit

---
+++ lib/App/summ.pm
@@ -0,0 +1,18 @@
+package App::summ;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Print summary statistics of a series of numbers
+
+=head1 SYNOPSIS
+
+See the command-line script L<summ>.
+
+
+=head1 SEE ALSO
+
+L<List::Util>, L<Statistics::Basic>
+
+=cut
